abbcf027462433e89ae5fc714201a052aeae6506
Jareer Abdullah <jforeverything2007@gmail.com>
2023-03-04 23:18:52 +0100

    Build scripts fixed

--- scripts/build-port.sh
+++ scripts/build-port.sh
@@ -1,6 +1,8 @@
 ## Build compressed and optimized for portable use.
 ## Variables
-DIR=bin/portable
+DIR=bin
+SUBDIR=portable
+FULLDIR=$DIR/$SUBDIR
 NAME=comx
 LD_FLAGS='-s -w'
 UPX_CMDS='--lzma --best'
@@ -10,20 +12,24 @@ if [ ! -d "./$DIR" ]; then
     mkdir "./$DIR"
 fi
 
+if [ ! -d "./$FULLDIR" ]; then
+    mkdir "./$FULLDIR"
+fi
+
 ## Build For Windows
 set GOOS=windows
 set GOHOSTOS=windows
-go build -o ./$DIR/$NAME-win64-portable.exe -ldflags="$LD_FLAGS"
-upx ./$DIR/$NAME-win64 $UPX_CMDS
+go build -o ./$FULLDIR/$NAME-win64-portable.exe -ldflags="$LD_FLAGS"
+upx ./$FULLDIR/$NAME-win64-portable.exe $UPX_CMDS
 
 ## Build For Linux Distro(s)
 set GOOS=linux
 set GOHOSTOS=linux
-go build -o ./$DIR/$NAME-linux64-portable -ldflags="$LD_FLAGS"
-upx ./$DIR/$NAME-linux64 $UPX_CMDS
+go build -o ./$FULLDIR/$NAME-linux64-portable -ldflags="$LD_FLAGS"
+upx ./$FULLDIR/$NAME-linux64-portable $UPX_CMDS
 
 ## Build for Mac
 set GOOS=darwin
 setGOHOSTOS=darwin
-go build -o ./$DIR/$NAME-mac64-portable -ldflags="$LD_FLAGS"
-upx ./$DIR/$NAME-mac64 $UPX_CMDS
+go build -o ./$FULLDIR/$NAME-mac64-portable -ldflags="$LD_FLAGS"
+upx ./$FULLDIR/$NAME-mac64-portable $UPX_CMDS
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,6 @@
 ## Variables
-DIR=bin/normal
+DIR=bin
+SUBDIR=normal
 NAME=comx
 
 ## Create the target directory if does't exist
@@ -7,16 +8,20 @@ if [ ! -d "./$DIR" ]; then
     mkdir "./$DIR"
 fi
 
+if [ ! -d "./$DIR/$SUBDIR" ]; then
+    mkdir "./$DIR/$SUBDIR"
+fi
+
 ## Build For Windows
 set GOOS=windows
 set GOHOSTOS=windows
-go build -o ./$DIR/$NAME-win64.exe
+go build -o ./$DIR/$SUBDIR/$NAME-win64.exe
 ## Build For Linux Distro(s)
 set GOOS=linux
 set GOHOSTOS=linux
-go build -o ./$DIR/$NAME-linux64
+go build -o ./$DIR/$SUBDIR/$NAME-linux64
 
 ## Build for Mac
 set GOOS=darwin
 setGOHOSTOS=darwin
-go build -o ./$DIR/$NAME-mac64
+go build -o ./$DIR/$SUBDIR/$NAME-mac64
