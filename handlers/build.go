fe0e1cdca29d480604607b114d8f595f7d227876
Jareer Abdullah <jforeverything2007@gmail.com>
2023-03-07 10:45:38 +0100

    Update build.go

--- handlers/build.go
+++ handlers/build.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"regexp"
+	"strings"
 
 	"github.com/urfave/cli/v2"
 )
@@ -14,6 +16,7 @@ func BuildHandler(cCtx *cli.Context) error {
 		return nil
 	} else {
 		var main_file string
+		var file_output string
 
 		fn := cCtx.Args().Get(0)
 
@@ -33,11 +36,25 @@ func BuildHandler(cCtx *cli.Context) error {
 			main_dir = store.MainDir
 		}
 
+		cwd, c_err := os.Getwd()
+
+		if c_err != nil {
+			utils.PrintError("Something went wrong while getting the CWD path, try running as root")
+			return nil
+		}
+
 		output := cCtx.String("output")
 
 		if len(output) == 0 || output == " " {
 			utils.PrintError("Please enter a valid output path using the '--output' argument.")
 			return nil
+		}
+
+		if strings.HasPrefix(output, "./") {
+			re, _ := regexp.Compile("/+")
+			file_output = re.ReplaceAllLiteralString(fmt.Sprintf("%v/%v", cwd, strings.Trim(output, "./")), "/")
+		} else {
+			file_output = output
 		}
 
 		compiler := store.SelectedCompiler
@@ -47,20 +64,12 @@ func BuildHandler(cCtx *cli.Context) error {
 			return nil
 		}
 
-		cwd, c_err := os.Getwd()
-
-		if c_err != nil {
-			utils.PrintError("Something went wrong while getting the CWD path, try running as root")
-			return nil
-		}
-
 		build_path := fmt.Sprintf("%v/%s/%s", cwd, main_dir, main_file)
-		out_path := fmt.Sprintf("%v/%v", cwd, output)
 
-		utils.PrintInfo(fmt.Sprintf(`Building using command: %v %v %v`, compiler, build_path, out_path))
+		utils.PrintInfo(fmt.Sprintf(`Building using command: %v %v -o %v`, compiler, build_path, file_output))
 		utils.PrintInfo("If build doesn't succeed, select a different compiler using the 'select-compiler' command.")
 
-		cmd := exec.Command(compiler, build_path, "-o", out_path)
+		cmd := exec.Command(compiler, build_path, "-o", file_output)
 		outp, err := cmd.CombinedOutput()
 
 		if err != nil {
